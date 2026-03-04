daf5d199af86ab431bfa7314ccda3ce50da23bca
Andre <106368574+seigfrieds@users.noreply.github.com>
2026-03-03 23:45:48 -0600

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,5 +2,9 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
